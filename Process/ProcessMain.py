6b412f138f6063eee1b9061178650dd208ba229d
Etenanl <40615767+Etenanl@users.noreply.github.com>
2022-04-20 15:58:46 +0800

    Process输出逻辑

--- Process/ProcessMain.py
+++ Process/ProcessMain.py
@@ -1,12 +1,15 @@
 import os
+import csv
 from Process import _Process
 if __name__ == '__main__':
-    result_dir = "/Users/tesla/Documents/实验室/distributed_sketch/simulation/Source/Result"
-    analyze_dir = "/Users/tesla/Documents/实验室/distributed_sketch/simulation/Source/Analyze"
+    result_dir = "..\\Source\\Result"
+    analyze_dir = "..\\Source\\Analyze"
     p = _Process()
     #直接把所有的路径记录下来
+    whole_result = []
     all_relative_dir = ["CU", "Distribute", "Common" + os.sep + "Core", "Common" + os.sep + "Edge", "Common" + os.sep + "Every"]
     for dir_name in all_relative_dir:
+        whole_result.append([])
         #绝对路径
         input_ab_dir = result_dir + os.sep + dir_name
         output_ab_dir = analyze_dir + os.sep + dir_name
@@ -18,7 +21,11 @@ if __name__ == '__main__':
         print(output_ab_dir)
         p.clear()
         p.initialize(input_ab_dir)
-        p.write_ARE(output_ab_dir + os.sep + "ARE.txt")
-        p.write_WMRE(output_ab_dir + os.sep + "WMRE.txt")
-        p.write_F1Score(output_ab_dir + os.sep + "F1Score.txt", 0.1)
-        p.write_entropy_RE(output_ab_dir + os.sep + "RE.txt")
+        whole_result[-1].append(dir_name+":    "+p.write_ARE(output_ab_dir + os.sep + "ARE.txt"))
+        whole_result[-1].append(":    "+p.write_WMRE(output_ab_dir + os.sep + "WMRE.txt"))
+        whole_result[-1].append(":    "+p.write_F1Score(output_ab_dir + os.sep + "F1Score.txt", 0.1))
+        whole_result[-1].append(":    "+p.write_entropy_RE(output_ab_dir + os.sep + "RE.txt"))
+    with open(analyze_dir+"\\result.csv","w") as file:
+        writer = csv.writer(file)
+        writer.writerows(whole_result)
+        file.close()
